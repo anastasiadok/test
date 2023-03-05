23f9d0b6c84d2fc5b4de252aa816d47ff22b2dcf
adokuchaeva11 <adokuchaeva11@gmail.com>
2023-03-05 13:26:55 +0300

    Add project files.

---
+++ WebApplication3/Models/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication3.Models.Interfaces
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int id);
+        IEnumerable<User> GetUsersByRole(string role);
+        User GetUserByEmail(string email);
+    }
+}
